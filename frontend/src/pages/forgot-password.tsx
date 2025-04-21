2a8018319ac3bb016f54a6df1ec5f6504d58c6cc
Elton <elton1314@hotmail.com>
2025-04-21 20:53:19 -0300

    Add forgot password functionality with email input and submission handling

--- frontend/src/pages/forgot-password.tsx
+++ frontend/src/pages/forgot-password.tsx
@@ -1,8 +1,24 @@
 "use client";
 
+import api from "@/services/api";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function ForgotPasswordPage() {
+  const [email, setEmail] = useState("");
+
+  const submitForm = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      await api.post("/user/forgot-password", {
+        email,
+      });
+      alert("Password reset link sent to your email.");
+    } catch {
+      alert("Error: Unable to send password reset link.");
+    }
+  };
+
   return (
     <div className="hold-transition login-page" style={{ minHeight: "100vh" }}>
       <div className="login-box">
@@ -28,6 +44,8 @@ export default function ForgotPasswordPage() {
                   className="form-control"
                   placeholder="Email"
                   required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
                 <div className="input-group-append">
                   <div className="input-group-text">
@@ -41,6 +59,7 @@ export default function ForgotPasswordPage() {
                   <button
                     type="submit"
                     className="btn btn-primary btn-block col-12"
+                    onClick={(event) => submitForm(event)}
                   >
                     Request new password
                   </button>
