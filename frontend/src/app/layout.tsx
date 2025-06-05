61cf0948b0e5f8d0283e9a85bd1e8570cad11109
Elton Marques <elton1314@hotmail.com>
2025-06-05 20:53:14 -0300

    refactor(frontend): change titles from my-associate to saas-template

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,28 +2,28 @@ import type { Metadata } from "next";
 import Script from "next/script";
 
 export const metadata: Metadata = {
-  title: "My Associate",
-  description: "Your AI Assistent",
+	title: "SaaS Template",
+	description: "Template for SaaS",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <head></head>
-      <body className="">
-        <Script
-          async
-          id="google-adsense"
-          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${process.env.NEXT_GOOGLE_ADSENSE}`}
-          crossOrigin="anonymous"
-        >
-          <div className="wrapper">{children}</div>
-        </Script>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head></head>
+			<body className="">
+				<Script
+					async
+					id="google-adsense"
+					src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${process.env.NEXT_GOOGLE_ADSENSE}`}
+					crossOrigin="anonymous"
+				>
+					<div className="wrapper">{children}</div>
+				</Script>
+			</body>
+		</html>
+	);
 }
--- frontend/src/components/Dashboard/Footer/index.tsx
+++ frontend/src/components/Dashboard/Footer/index.tsx
@@ -2,35 +2,35 @@ import AuthContext from "@/contexts/AuthContext";
 import React, { useContext } from "react";
 
 const Footer: React.FC = () => {
-  const currentYear = new Date().getFullYear();
-  const { user } = useContext(AuthContext);
+	const currentYear = new Date().getFullYear();
+	const { user } = useContext(AuthContext);
 
-  return (
-    <>
-      {user && user.plan === "free" ? (
-        <>
-          <footer className="py-2 px-4 bg-gray-100 dark:bg-zinc-800 text-justify align-bottom text-sm text-gray-500 dark:text-gray-400">
-            <div className="pt-22 flex flex-row justify-between">
-              <strong>&copy; {currentYear} My Associate</strong>
-              <div></div>
-              <div className="float-right d-none d-sm-inline-block">
-                <b>Versão</b> 0.1.0
-              </div>
-            </div>
-          </footer>
-        </>
-      ) : (
-        <>
-          <footer className="py-1 px-4 bg-gray-100 dark:bg-zinc-800 text-justify text-sm text-gray-500 dark:text-gray-400">
-            <strong>&copy; {currentYear} My Associate</strong>
-            <div className="float-right d-none d-sm-inline-block">
-              <b>Versão</b> 0.1.0
-            </div>
-          </footer>
-        </>
-      )}
-    </>
-  );
+	return (
+		<>
+			{user && user.plan === "free" ? (
+				<>
+					<footer className="py-2 px-4 bg-gray-100 dark:bg-zinc-800 text-justify align-bottom text-sm text-gray-500 dark:text-gray-400">
+						<div className="pt-22 flex flex-row justify-between">
+							<strong>&copy; {currentYear} SaaS Template</strong>
+							<div></div>
+							<div className="float-right d-none d-sm-inline-block">
+								<b>Versão</b> 1.0.0
+							</div>
+						</div>
+					</footer>
+				</>
+			) : (
+				<>
+					<footer className="py-1 px-4 bg-gray-100 dark:bg-zinc-800 text-justify text-sm text-gray-500 dark:text-gray-400">
+						<strong>&copy; {currentYear} SaaS Template</strong>
+						<div className="float-right d-none d-sm-inline-block">
+							<b>Versão</b> 1.0.0
+						</div>
+					</footer>
+				</>
+			)}
+		</>
+	);
 };
 
 export default Footer;
