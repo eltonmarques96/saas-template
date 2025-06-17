cb28d488903f7c7af998b6f86b5462d54dc0c673
eltonmarques96 <elton1314@hotmail.com>
2025-06-17 14:56:58 -0300

    feat(backend): add method to auth user

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import {
   Body,
   Controller,
@@ -10,23 +11,67 @@ import {
 import { AuthService } from './auth.service';
 
 import { Public } from '@/metadata';
+import { AuthUserDTO } from './dto/auth-user.dto';
+import {
+  ApiBody,
+  ApiForbiddenResponse,
+  ApiHeader,
+  ApiOkResponse,
+  ApiOperation,
+  ApiResponse,
+} from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Public()
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        email: {
+          type: 'string',
+        },
+        password: {
+          type: 'string',
+        },
+      },
+      required: ['email', 'password'],
+    },
+  })
+  @ApiResponse({ status: 200, description: 'User authenticated' })
+  @ApiForbiddenResponse({ description: 'Forbidden.' })
+  @ApiOperation({ summary: 'Authenticated User' })
   @HttpCode(HttpStatus.OK)
   @Post('login')
-  signIn(@Body() signInDto: Record<string, any>) {
-    return this.authService.signIn(signInDto.email, signInDto.password);
+  async signIn(@Body() signInDto: Record<string, any>): Promise<AuthUserDTO> {
+    return await this.authService.signIn(signInDto.email, signInDto.password);
   }
 
+  @ApiForbiddenResponse({ description: 'Forbidden.' })
+  @ApiOperation({ summary: 'Get authenticated user info' })
   @Get('profile')
-  getProfile(@Request() req): Promise<{ id: string; email: string }> {
-    return Promise.resolve({
-      id: req.user.id,
-      email: req.user.email,
-    });
+  @ApiHeader({
+    name: 'Authorization',
+    description: 'Bearer Token',
+  })
+  @ApiOkResponse({
+    description: 'Return User Info',
+  })
+  async getProfile(@Request() req): Promise<AuthUserDTO> {
+    try {
+      const user = await this.authService.profile(req.data.sub);
+      const response = new AuthUserDTO(200, req.token, user);
+      return response;
+    } catch (error: any) {
+      const response = new AuthUserDTO(
+        HttpStatus.UNAUTHORIZED,
+        null,
+        null,
+        typeof error.message === 'string' ? error.message : 'Unauthorized',
+      );
+      return response;
+    }
   }
 }
