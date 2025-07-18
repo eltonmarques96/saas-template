5fdd69b978ba7aecba090d60120313a6ba8708bf
Elton Marques <elton1314@hotmail.com>
2025-07-18 15:26:53 -0300

    feat(mobile): add cookies to handle with csrf

--- mobile/services/api.ts
+++ mobile/services/api.ts
@@ -1,6 +1,37 @@
-import axios from "axios";
+import axios, { AxiosHeaders, InternalAxiosRequestConfig } from "axios";
 const api = axios.create({
   baseURL: process.env.EXPO_PUBLIC_API_URL || "http://192.168.15.12:8001",
 });
+
+api.interceptors.request.use(
+  async (config: InternalAxiosRequestConfig) => {
+    const method = config.method?.toLowerCase();
+    const METHODS_REQUIRING_CSRF = ["post", "put", "patch", "delete"];
+
+    // Se o método exige CSRF
+    if (method && METHODS_REQUIRING_CSRF.includes(method)) {
+      try {
+        // Requisição para obter o token
+        const { data: csrfData } = await api.get("/auth/csrf-token");
+        const csrfToken = csrfData?.csrfToken;
+
+        if (!config.headers) {
+          config.headers = new AxiosHeaders(); // ou {}
+        }
+
+        // Define o cabeçalho CSRF
+        if (csrfToken) {
+          config.headers.set("X-CSRF-Token", csrfToken);
+        }
+      } catch (err) {
+        console.warn("Erro ao buscar token CSRF:", err);
+        throw err;
+      }
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
